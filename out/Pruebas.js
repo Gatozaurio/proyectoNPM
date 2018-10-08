abb4cf4d728c950abf91c34a57911c12da039df7
Gatozaurio <tavoraromero@gmail.com>
2018-10-08 20:19:34 +0200

    Primer commit

---
+++ out/Pruebas.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var s = new String('rafa');
+var longitud = s.length; // Printea la longitud
+var pos = s.indexOf("fa"); // Printea el índice
+// console.log(pos);
+
+var a = [1, 2, 3, 4, 5, 6];
+a["color"] = "azul"; // Añade una propiedad al array
+a.color;
+a.push(7); // Añade 7 al array
+delete a[2]; // Elimina 2 del array
+
+var nombre = "Alberto";
+var apellido = "Távora";
+
+//console.log(a);
+
+// Plantilla
+console.log("Mi nombre es " + nombre + " y mi apellido " + apellido);
